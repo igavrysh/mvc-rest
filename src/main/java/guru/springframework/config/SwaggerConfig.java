e301fb9219aa42c188af4e28a833a10458018826
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-09-15 07:54:57 -0700

    Add swagger resources manual loader

--- src/main/java/guru/springframework/config/SwaggerConfig.java
+++ src/main/java/guru/springframework/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package guru.springframework.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig { //extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket api() {
@@ -23,4 +25,15 @@ public class SwaggerConfig {
                 .build()
                 .pathMapping("/");
     }
+
+    /*
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+     */
 }
